5e29aa8bd78b27471d8d871e6cc353685e53f5b1
FifthWit <fifthwitbusiness@gmail.com>
2025-01-07 17:33:52 -0600

    Edited sidebar with footer links

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -30,6 +30,8 @@ import {
     Settings,
     CircleHelp,
     Sparkles,
+    Github,
+
 } from "lucide-react"
 import { shouldShowProgress } from "@/stores/progress/utils";
 import { MediaItem } from "@/utils/mediaTypes";
@@ -87,6 +89,21 @@ export function AppSidebar() {
 
     const footerItems: footerItem[] = [
         {
+            icon: <Sparkles />,
+            title: "Discover",
+            link: "/Discover",
+        },
+        {
+            icon: <Github />,
+            title: "Contribute ˶ᵔ ᵕ ᵔ˶",
+            link: "https://github.com/FifthWit/smov-static"
+        },
+        {
+            icon: <DiscordIcon />,
+            title: "Discord",
+            link: "https://docs.undi.rest/links/discord"  
+        },
+        {
             icon: <Settings />,
             title: "Settings",
             link: "/settings",
@@ -95,11 +112,6 @@ export function AppSidebar() {
             icon: <CircleHelp />,
             title: "About us",
             link: "/about",
-        },
-        {
-            icon: <Sparkles />,
-            title: "Discover",
-            link: "/Discover",
         },
     ]
 
@@ -119,55 +131,63 @@ export function AppSidebar() {
                 </SidebarMenuButton>
             </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup>
+                {subGroups.some(group => group.media.length > 0) && (
+                    <SidebarGroup>
                         <SidebarGroupContent>
                             <SidebarMenu>
                                 {/* Watched Media Collapsible sub-menu */}
-                                {subGroups.map((group) =>  (
-                                <Collapsible defaultOpen className="group/collapsible">
-                                    <SidebarMenuItem>
-                                        <CollapsibleTrigger asChild>
-                                            <SidebarMenuButton>
-                                                {group.icon}
-                                                {group.title}
-                                                <ChevronUp className="ml-auto group-data-[state=open]/collapsible:rotate-180 duration-200 transition-all" />
-                                            </SidebarMenuButton>
-                                        </CollapsibleTrigger>
-                                        <CollapsibleContent>
-                                            <SidebarMenuSub>
-                                                {group.media.map((media) => (
-                                                    <SidebarMenuSubItem key={media.title} className="">
-                                                        <SidebarMenuSubButton asChild className="py-4">
-                                                            <Link to={`/media/${encodeURIComponent(mediaItemToId(media))}`} className="flex items-center justify-between">
-                                                                {media.title}
-                                                                {media.type === "movie" ? <Film />  : <MonitorPlay />}
-                                                            </Link>
-                                                        </SidebarMenuSubButton>
-                                                    </SidebarMenuSubItem>
-                                                ))}
-                                            </SidebarMenuSub>
-                                        </CollapsibleContent>
-                                    </SidebarMenuItem>
-                                </Collapsible>
+                                {subGroups.map((group) => (
+                                    <Collapsible defaultOpen className="group/collapsible" key={group.title}>
+                                        <SidebarMenuItem>
+                                            <CollapsibleTrigger asChild>
+                                                <SidebarMenuButton>
+                                                    {group.icon}
+                                                    {group.title}
+                                                    <ChevronUp className="ml-auto group-data-[state=open]/collapsible:rotate-180 duration-200 transition-all" />
+                                                </SidebarMenuButton>
+                                            </CollapsibleTrigger>
+                                            <CollapsibleContent>
+                                                {group.media.length > 0 ? (
+                                                    <SidebarMenuSub>
+                                                        {group.media.map((media) => (
+                                                            <SidebarMenuSubItem key={media.title} className="">
+                                                                <SidebarMenuSubButton asChild className="py-4">
+                                                                    <Link to={`/media/${encodeURIComponent(mediaItemToId(media))}`} className="flex items-center justify-between">
+                                                                        {media.title}
+                                                                        {media.type === "movie" ? <Film /> : <MonitorPlay />}
+                                                                    </Link>
+                                                                </SidebarMenuSubButton>
+                                                            </SidebarMenuSubItem>
+                                                        ))}
+                                                    </SidebarMenuSub>
+                                                ) : (
+                                                    <div className="p-4 text-center text-muted-foreground">No items available</div>
+                                                )}
+                                            </CollapsibleContent>
+                                        </SidebarMenuItem>
+                                    </Collapsible>
                                 ))}
                             </SidebarMenu>
                         </SidebarGroupContent>
-                </SidebarGroup>
+                    </SidebarGroup>
+                )}
             </SidebarContent>
             <SidebarFooter>
                 <SidebarMenu className="*:mt-1">
                     {footerItems.map((item) => (
-                        <SidebarMenuButton asChild isActive={window.location.pathname === item.link} className="transition-all duration-300">
+                        <SidebarMenuButton asChild isActive={window.location.pathname === item.link} className="transition-all duration-300 bg-accent/30">
                             <Link to={item.link}>{item.icon} {item.title}</Link>
                         </SidebarMenuButton>
                     ))}
-                    {loggedIn ? <UserAvatar withName className="bg-secondary rounded-lg p-2" /> : <NoUserAvatar />}
+                    {loggedIn ? <UserAvatar withName className="bg-accent/30 rounded-lg p-2" /> : <NoUserAvatar />}
                 </SidebarMenu>
             </SidebarFooter>
         </Sidebar>
     )
 }
 
-function useMemo(arg0: () => import("@/stores/progress").ProgressMediaItem, arg1: any[]) {
-    throw new Error("Function not implemented.")
+function DiscordIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 640 512"><path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/></svg>
+    )
 }
