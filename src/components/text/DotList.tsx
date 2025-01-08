1cb841588e17bc08e2ef39c8067b16a38e33b687
FifthWit <fifthwitbusiness@gmail.com>
2025-01-07 23:09:53 -0600

    updated muted-foreground and had dotlist use it

--- src/components/text/DotList.tsx
+++ src/components/text/DotList.tsx
@@ -5,7 +5,7 @@ export interface DotListProps {
 
 export function DotList(props: DotListProps) {
   return (
-    <p className={`font-semibold text-type-secondary ${props.className || ""}`}>
+    <p className={`font-semibold text-muted-foreground ${props.className || ""}`}>
       {props.content.map((item, index) => (
         <span key={item}>
           {index !== 0 ? (
