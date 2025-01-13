73016f40ff31a7ee312c94bf2d55ae6dabe14831
FifthWit <fifthwitbusiness@gmail.com>
2025-01-13 12:10:15 -0600

    styled toggle

--- src/components/buttons/Toggle.tsx
+++ src/components/buttons/Toggle.tsx
@@ -7,13 +7,13 @@ export function Toggle(props: { onClick?: () => void; enabled?: boolean }) {
       onClick={props.onClick}
       className={classNames(
         "w-11 h-6 p-1 rounded-full grid transition-colors duration-100 group/toggle tabbable",
-        props.enabled ? "bg-buttons-toggle" : "bg-buttons-toggleDisabled",
+        props.enabled ? "bg-primary" : "bg-input"
       )}
     >
       <div className="relative w-full h-full">
         <div
           className={classNames(
-            "scale-90 group-hover/toggle:scale-100 h-full aspect-square rounded-full bg-white absolute transition-all duration-100",
+            "scale-90 group-hover/toggle:scale-100 h-full aspect-square rounded-full bg-foreground absolute transition-all duration-100",
             props.enabled ? "left-full transform -translate-x-full" : "left-0",
           )}
         />
