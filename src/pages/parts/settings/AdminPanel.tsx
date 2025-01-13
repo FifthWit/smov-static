b08f333182d902ecbaee563c5f5f4641945c15db
FifthWit <fifthwitbusiness@gmail.com>
2025-01-13 10:59:18 -0600

    migrated AdminPanel

--- src/pages/parts/settings/AdminPanel.tsx
+++ src/pages/parts/settings/AdminPanel.tsx
@@ -1,30 +1,30 @@
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
-import { Button } from "@/components/buttons/Button";
-import { SolidSettingsCard } from "@/components/layout/SettingsCard";
-import { Heading3 } from "@/components/utils/Text";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+    CardDescription
+  } from "@/components/ui/card"
 
 export function AdminPanelPart() {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
   return (
-    <div>
-      <SolidSettingsCard
-        paddingClass="px-6 py-12"
-        className="grid grid-cols-2 gap-12 mt-5"
-      >
-        <div>
-          <Heading3>{t("settings.account.admin.title")}</Heading3>
-          <p className="text-type-text">{t("settings.account.admin.text")}</p>
-        </div>
-        <div className="flex justify-end items-center">
-          <Button theme="purple" onClick={() => navigate("/admin")}>
-            {t("settings.account.admin.button")}
-          </Button>
-        </div>
-      </SolidSettingsCard>
-    </div>
+        <Card>
+            <CardHeader>
+              <CardTitle>{t("settings.account.admin.title")}</CardTitle>
+              <CardDescription>{t("settings.account.admin.text")}</CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-end items-center">
+              <Button variant="default" onClick={() => navigate("/admin")}>
+                {t("settings.account.admin.button")}
+              </Button>
+            </CardFooter>
+        </Card>
   );
 }
