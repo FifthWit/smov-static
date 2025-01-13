17d8db0d9b33534a3993fed8667d09e1a2da7fc1
FifthWit <fifthwitbusiness@gmail.com>
2025-01-12 23:23:17 -0600

    Updated <AccountEditPart /> to use shadcn styles and just better styling

--- src/pages/parts/settings/AccountEditPart.tsx
+++ src/pages/parts/settings/AccountEditPart.tsx
@@ -1,14 +1,29 @@
 import { useTranslation } from "react-i18next";
 
 import { Avatar } from "@/components/Avatar";
-import { Button } from "@/components/buttons/Button";
+import { Button } from "@/components/ui/button";
 import { Icon, Icons } from "@/components/Icon";
-import { SettingsCard } from "@/components/layout/SettingsCard";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+  } from "@/components/ui/card"
+  
 import { useModal } from "@/components/overlays/Modal";
 import { AuthInputBox } from "@/components/text-inputs/AuthInputBox";
 import { UserIcons } from "@/components/UserIcon";
+import { ColorPicker } from "@/components/form/ColorPicker";
 import { useAuth } from "@/hooks/auth/useAuth";
-import { ProfileEditModal } from "@/pages/parts/settings/ProfileEditModal";
+import { IconPicker } from "@/components/form/IconPicker";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogClose,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 
 export function AccountEditPart(props: {
   deviceName: string;
@@ -25,21 +40,11 @@ export function AccountEditPart(props: {
   const profileEditModal = useModal("profile-edit");
 
   return (
-    <SettingsCard paddingClass="px-8 py-10" className="!mt-8 w-full">
-      <ProfileEditModal
-        id={profileEditModal.id}
-        close={profileEditModal.hide}
-        colorA={props.colorA}
-        setColorA={props.setColorA}
-        colorB={props.colorB}
-        setColorB={props.setColorB}
-        userIcon={props.userIcon}
-        setUserIcon={props.setUserIcon}
-      />
-      <div className="grid lg:grid-cols-[auto,1fr] gap-8">
-        <div>
-          <Avatar
-            profile={{
+    <Card className="w-fit">
+        <CardHeader className="flex flex-row justify-center">
+            <div className="p-4 justify-center">
+                <Avatar
+                  profile={{
               colorA: props.colorA,
               colorB: props.colorB,
               icon: props.userIcon,
@@ -47,39 +52,81 @@ export function AccountEditPart(props: {
             iconClass="text-5xl"
             sizeClass="w-32 h-32"
             bottom={
-              <button
-                type="button"
-                className="tabbable text-xs flex gap-2 items-center bg-editBadge-bg text-editBadge-text hover:bg-editBadge-bgHover py-1 px-3 rounded-full cursor-pointer"
-                onClick={profileEditModal.show}
-              >
-                <Icon icon={Icons.EDIT} />
-                {t("settings.account.accountDetails.editProfile")}
-              </button>
-            }
-          />
-        </div>
-        <div>
-          <div className="space-y-8 max-w-xs">
-            <AuthInputBox
-              label={
-                t("settings.account.accountDetails.deviceNameLabel") ??
-                undefined
-              }
-              placeholder={
-                t("settings.account.accountDetails.deviceNamePlaceholder") ??
-                undefined
-              }
-              value={props.deviceName}
-              onChange={(value) => props.setDeviceName(value)}
-            />
-            <div className="flex space-x-3">
-              <Button theme="danger" onClick={logout}>
-                {t("settings.account.accountDetails.logoutButton")}
-              </Button>
+                <Dialog>
+                    <DialogTrigger>
+                        <button
+                            type="button"
+                            className="tabbable text-xs flex gap-2 items-center bg-secondary text-foreground hover:bg-editBadge-bgHover py-1 px-3 rounded-full cursor-pointer"
+                            onClick={profileEditModal.show}
+                        >
+                            <Icon icon={Icons.EDIT} />
+                            {t("settings.account.accountDetails.editProfile")}
+                        </button>
+                    </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                              <DialogTitle className="!mt-0 !mb-0">
+                                {t("settings.account.profile.title")}
+                              </DialogTitle>
+                              <Avatar
+                                profile={{
+                                  colorA: props.colorA,
+                                  colorB: props.colorB,
+                                  icon: props.userIcon,
+                                }}
+                                iconClass="text-2xl"
+                                sizeClass="w-12 h-12"
+                              />
+                              </DialogHeader>
+                            <div className="space-y-6">
+                              <ColorPicker
+                                label={t("settings.account.profile.firstColor")}
+                                value={props.colorA}
+                                onInput={props.setColorA}
+                              />
+                              <ColorPicker
+                                label={t("settings.account.profile.secondColor")}
+                                value={props.colorB}
+                                onInput={props.setColorB}
+                              />
+                              <IconPicker
+                                label={t("settings.account.profile.userIcon")}
+                                value={props.userIcon}
+                                onInput={props.setUserIcon}
+                              />
+                            </div>
+                            <div className="flex justify-center mt-8">
+                                <DialogClose asChild>
+                                  <Button className="!px-20">
+                                    {t("settings.account.profile.finish")}
+                                  </Button>
+                                </DialogClose>
+                            </div>
+                </DialogContent>
+              </Dialog>
+                  }
+                />
             </div>
-          </div>
-        </div>
-      </div>
-    </SettingsCard>
+        </CardHeader>
+        <CardContent>
+            <AuthInputBox
+                label={
+                  t("settings.account.accountDetails.deviceNameLabel") ??
+                  undefined
+                }
+                placeholder={
+                  t("settings.account.accountDetails.deviceNamePlaceholder") ??
+                  undefined
+                }
+                value={props.deviceName}
+                onChange={(value) => props.setDeviceName(value)}
+            />
+        </CardContent>
+        <CardFooter>
+            <Button variant="destructive" onClick={logout} className="w-full">
+                {t("settings.account.accountDetails.logoutButton")}
+            </Button>
+        </CardFooter>
+    </Card>
   );
 }
