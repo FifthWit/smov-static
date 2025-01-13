f7a6226f02392b41c0eae8b037f2b8c037302cf8
FifthWit <fifthwitbusiness@gmail.com>
2025-01-12 23:23:46 -0600

    Updated to use Shadcn

--- src/components/text-inputs/AuthInputBox.tsx
+++ src/components/text-inputs/AuthInputBox.tsx
@@ -24,7 +24,7 @@ export function AuthInputBox(props: {
         onChange={props.onChange}
         placeholder={props.placeholder}
         passwordToggleable={props.passwordToggleable}
-        className="w-full flex-1 bg-authentication-inputBg px-4 py-3 text-search-text focus:outline-none rounded-lg placeholder:text-gray-700"
+        className=""
       />
     </div>
   );
