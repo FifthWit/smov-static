3ddd9280589a05c2e3c26a7ac82b635386338daa
FifthWit <fifthwitbusiness@gmail.com>
2025-01-12 23:37:28 -0600

    Hopefully last change to the accounteditpart component

--- src/pages/parts/settings/AccountEditPart.tsx
+++ src/pages/parts/settings/AccountEditPart.tsx
@@ -8,6 +8,8 @@ import {
     CardContent,
     CardFooter,
     CardHeader,
+    CardTitle,
+    CardDescription
   } from "@/components/ui/card"
   
 import { useModal } from "@/components/overlays/Modal";
@@ -41,72 +43,77 @@ export function AccountEditPart(props: {
 
   return (
     <Card className="w-fit">
-        <CardHeader className="flex flex-row justify-center">
-            <div className="p-4 justify-center">
-                <Avatar
-                  profile={{
-              colorA: props.colorA,
-              colorB: props.colorB,
-              icon: props.userIcon,
-            }}
-            iconClass="text-5xl"
-            sizeClass="w-32 h-32"
-            bottom={
-                <Dialog>
-                    <DialogTrigger>
-                        <button
-                            type="button"
-                            className="tabbable text-xs flex gap-2 items-center bg-secondary text-foreground hover:bg-editBadge-bgHover py-1 px-3 rounded-full cursor-pointer"
-                            onClick={profileEditModal.show}
-                        >
-                            <Icon icon={Icons.EDIT} />
-                            {t("settings.account.accountDetails.editProfile")}
-                        </button>
-                    </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                              <DialogTitle className="!mt-0 !mb-0">
-                                {t("settings.account.profile.title")}
-                              </DialogTitle>
-                              <Avatar
-                                profile={{
-                                  colorA: props.colorA,
-                                  colorB: props.colorB,
-                                  icon: props.userIcon,
-                                }}
-                                iconClass="text-2xl"
-                                sizeClass="w-12 h-12"
-                              />
-                              </DialogHeader>
-                            <div className="space-y-6">
-                              <ColorPicker
-                                label={t("settings.account.profile.firstColor")}
-                                value={props.colorA}
-                                onInput={props.setColorA}
-                              />
-                              <ColorPicker
-                                label={t("settings.account.profile.secondColor")}
-                                value={props.colorB}
-                                onInput={props.setColorB}
-                              />
-                              <IconPicker
-                                label={t("settings.account.profile.userIcon")}
-                                value={props.userIcon}
-                                onInput={props.setUserIcon}
-                              />
-                            </div>
-                            <div className="flex justify-center mt-8">
-                                <DialogClose asChild>
-                                  <Button className="!px-20">
-                                    {t("settings.account.profile.finish")}
-                                  </Button>
-                                </DialogClose>
-                            </div>
-                </DialogContent>
-              </Dialog>
-                  }
-                />
-            </div>
+        <CardHeader>
+            <CardTitle>
+                {t("settings.account.title")}
+            </CardTitle>
+            <CardDescription className="flex flex-row justify-center">
+                <div className="p-4 justify-center">
+                    <Avatar
+                      profile={{
+                  colorA: props.colorA,
+                  colorB: props.colorB,
+                  icon: props.userIcon,
+                }}
+                iconClass="text-5xl"
+                sizeClass="w-32 h-32"
+                bottom={
+                    <Dialog>
+                        <DialogTrigger>
+                            <button
+                                type="button"
+                                className="tabbable text-xs flex gap-2 items-center bg-secondary text-foreground hover:bg-background py-1 px-3 rounded-full cursor-pointer"
+                                onClick={profileEditModal.show}
+                            >
+                                <Icon icon={Icons.EDIT} />
+                                {t("settings.account.accountDetails.editProfile")}
+                            </button>
+                        </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                                  <DialogTitle className="!mt-0 !mb-0">
+                                    {t("settings.account.profile.title")}
+                                  </DialogTitle>
+                                  <Avatar
+                                    profile={{
+                                      colorA: props.colorA,
+                                      colorB: props.colorB,
+                                      icon: props.userIcon,
+                                    }}
+                                    iconClass="text-2xl"
+                                    sizeClass="w-12 h-12"
+                                  />
+                                  </DialogHeader>
+                                <div className="space-y-6">
+                                  <ColorPicker
+                                    label={t("settings.account.profile.firstColor")}
+                                    value={props.colorA}
+                                    onInput={props.setColorA}
+                                  />
+                                  <ColorPicker
+                                    label={t("settings.account.profile.secondColor")}
+                                    value={props.colorB}
+                                    onInput={props.setColorB}
+                                  />
+                                  <IconPicker
+                                    label={t("settings.account.profile.userIcon")}
+                                    value={props.userIcon}
+                                    onInput={props.setUserIcon}
+                                  />
+                                </div>
+                                <div className="flex justify-center mt-8">
+                                    <DialogClose asChild>
+                                      <Button className="!px-20">
+                                        {t("settings.account.profile.finish")}
+                                      </Button>
+                                    </DialogClose>
+                                </div>
+                    </DialogContent>
+                  </Dialog>
+                      }
+                    />
+                </div>
+            </CardDescription>
         </CardHeader>
         <CardContent>
             <AuthInputBox
